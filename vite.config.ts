4f87b28df5c283e6624424350fc6080bc2be3c58
Ivan Kovrigin <vanya.kovrigin@gmail.com>
2026-01-09 17:33:56 +0700

    some fixes

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-   base: '/cv-page/',
-    resolve: {
+  base: '/cv-page/',
+  resolve: {
     alias: {
       models: path.resolve(__dirname, 'public/models'),
       shared: path.resolve(__dirname, 'src/shared'),
+      react: path.resolve(__dirname, 'node_modules/react'),
+      'react-dom': path.resolve(__dirname, 'node_modules/react-dom'),
     },
   },
 })
